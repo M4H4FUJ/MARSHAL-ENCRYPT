6dcf331a7310d0c0628c03fccf0d594a761b341d
dark boy (mahfuj) <142173084+M4H4FUJ@users.noreply.github.com>
2023-12-02 01:08:52 +0600

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,10 +1,13 @@
 import os
 os.system("clear")
 print("""\033[38;5;196m
- ██████  ███████ ███████ 
-██    ██ ██      ██      
-██    ██ █████   █████   
-██    ██ ██      ██      
- ██████  ██      ██      
+ 
+███    ███  █████  ██   ██ ███████ ██    ██      ██ 
+████  ████ ██   ██ ██   ██ ██      ██    ██      ██ 
+██ ████ ██ ███████ ███████ █████   ██    ██      ██ 
+██  ██  ██ ██   ██ ██   ██ ██      ██    ██ ██   ██ 
+██      ██ ██   ██ ██   ██ ██       ██████   █████  
+                                                    
+                                                         
 """)
 print("THIS TOOLS IS OFF FOR PERMANEMTLY\n\033[0mREPOSITORY :\033[38;5;46m https://github.com/COBRA-404-CYBER/CODE-X\n\x1b[38;5;208mUSE THIS REPOSITORY FOR ENCRYPT\n ")
